9b661fc6b5ed74fd42f7a73f6e84c6d914308be7
Sithu Naing <sithunaing8209@gmail.com>
2024-08-19 20:33:13 +0630

    current and exprie filter complete

--- src/pages/app/backoffice/expire/index.tsx
+++ src/pages/app/backoffice/expire/index.tsx
@@ -1,15 +1,16 @@
 import { useAppSelector } from "@/store/hooks";
 import { Box, Chip, ToggleButton, ToggleButtonGroup, Typography } from "@mui/material"
-import { Student } from "@prisma/client";
+import { PayAndEndDate, Student } from "@prisma/client";
 import { useEffect, useState } from "react";
 
 const ExpireStudentPage = () => {
-    const [ condition , setCondition ] = useState('all');
+    const [ condition , setCondition ] = useState('current');
     const [ selectedHostelId , setSelectedHostelId ] = useState<number>();
-    const [ filteredStudents , setFilteredStudents ] = useState<Student[]>([]);
+    const [ filteredPayments , setFilteredPayments ] = useState<PayAndEndDate[]>([]);
     const hostels = useAppSelector(store => store.hostel.items);
     const students = useAppSelector(store => store.student.items); // should check with payAndEndDate
-
+    const payAndEndDate = useAppSelector(store => store.payAndEndDate.items);
+    console.log(filteredPayments )
     useEffect(() => {
         if(hostels.length) {
             setSelectedHostelId(hostels[0].id);
@@ -18,14 +19,36 @@ const ExpireStudentPage = () => {
 
     useEffect(() => {
         if(condition && selectedHostelId) {
-            if(condition === "all") {
-                
-            } else if(condition === "current") {
-                
+            const now = new Date();
+            const currentTime = now.getTime();
+
+            if(condition === "current") {
+                const currentPayments = payAndEndDate.filter(item => {
+                    now.setFullYear(item.payYear , item.payMonth - 1 , item.payDate);
+                    const payTime = now.getTime();
+                    now.setFullYear(item.endYear , item.endMonth - 1 , item.endDate);
+                    const endTime = now.getTime();
+                    if(payTime <= currentTime && currentTime < endTime && !item.isDone ) {
+                        return item;
+                    }
+                });
+                const selectedHostelStudentIds = students.filter(item => item.hostelId === selectedHostelId).map(item => item.id);
+                const currentHostelPayments = currentPayments.filter(item => selectedHostelStudentIds.includes(item.studentId))
+                setFilteredPayments(currentHostelPayments);
+            
             } else if(condition === "expired") {
-                
+                const expiredPayments = payAndEndDate.filter(item => {
+                    now.setFullYear(item.endYear , item.endMonth - 1 , item.endDate);
+                    const endTime = now.getTime();
+                    if(  endTime <= currentTime && !item.isDone ) {
+                        return item;
+                    }
+                }).sort(( a , b ) => a.studentId - b.studentId );
+                const selectedHostelStudentIds = students.filter(item => item.hostelId === selectedHostelId).map(item => item.id);
+                const expiredHostelPayments = expiredPayments.filter(item => selectedHostelStudentIds.includes(item.studentId))
+                setFilteredPayments(expiredHostelPayments);
             }else if(condition === "done") {
-                
+                // here
             }
         }
     } , [ condition , selectedHostelId ])
@@ -40,7 +63,6 @@ const ExpireStudentPage = () => {
                   exclusive
                   onChange={(event , value) => setCondition(value)}
                 >
-                  <ToggleButton value="all">All</ToggleButton>
                   <ToggleButton value="current">Current</ToggleButton>
                   <ToggleButton value="expired">Expired</ToggleButton>
                   <ToggleButton value="done">Done</ToggleButton>
